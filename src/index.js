8c5a276f8e18e15c6fd07af1bf54a48a7ac3ca44
Anna Barsukova <Anna Barsukova>
2022-10-02 20:01:32 +0300

    add hw-2 (arr)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 // const currency = {
 //     usd: 'dollar',
 //     blr: 'bel rouble',
-//     rus: 'rus rouble',
+//     eur: 'euro',
 // }
 
-// // for (let key in currency){
-// //     console.log(key)
-// // }
+// let key = prompt('Введите валюту');
+//     console.log(currency[key])
+
+
+// let user = {
+//     name: 'Anna',
+//     age: 27
+// }
+
+
+// for (let key in user){
+//     console.log(user[key]) // вывод значений
+//     console.log(key) // вывод ключей
+
+// }
+
+// let descriptor = Object.defineProperty(user, 'age', {
+//     writable: false,
+//     enumerable: false,
+//     configurable: false
+// });
+
+// descriptor.age = 20;
+// console.log(user)
+
+// for (let key in currency){
+//     console.log(key)
+// }
 
 // let descriptor = Object.defineProperty(currency, 'usd', {
 //     writable: false,
@@ -56,8 +81,120 @@
 // console.log(sum(1,2,3))
 
 
-const min = (a, b) => {
-    return (a<b)?a:b
+// const min = (a, b) => {
+//     return (a<b)?a:b
 
+// }
+// console.log(min(2,5))
+
+
+let arr = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8];
+
+let sum = 0;
+
+function sumTree(array){
+    array.forEach((item) =>{
+        Array.isArray(item) ? sumTree(item) : sum +=item;
+    });
+    return sum;
 }
-console.log(min(2,5))
+console.log(sumTree(arr));
+
+
+
+
+
+
+// function sumTree(arr){
+//     let sum = 0;
+//     arr.forEach(function(item){
+//         if(Array.isArray(item) ){
+//             sumTree(item)
+//         }
+//         else{
+//             sum +=item
+//         } 
+//     });
+
+//     return sum;
+// }
+// console.log(sumTree(arr));
+
+
+//1
+// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+// let veg = ['sfsf', 'fggsfg', 'sgsggg', 'Морковка'];
+
+
+// let copyArr = (array) =>{
+//     return array.slice()
+// }
+// console.log(copyArr(vegetables))
+
+
+//2
+// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+
+// let str = (arr) =>{
+//     return arr.join()
+// }
+
+// let str2 = (arr) =>{
+//     return arr.toString()
+// }
+// console.log(str(vegetables))
+// console.log(str2(vegetables))
+
+//3???
+
+// let num = prompt('Введите число')
+
+
+// let colonOd = (num) =>{
+//     let vvodStr = num.toString()
+//     for (let i of num ){
+//         if ((i % 2 !== 0) && ((i-1)%2 !==0 )){
+//             num.splice(':')
+//         }
+        
+//     }
+//     return
+// }
+// console.log(colonOd(num))
+
+
+//4
+// const str = prompt('Введите слово', 'КаЖдЫй ОхОтНиК');
+// let upper = 'АБВГД';
+// let lower = 'абвгд';
+// let k = [];
+// let changeRegister = (str) =>{
+//     for (let i of str.length){
+//         if (upper.includes(i)){
+//             return k.toUpperCase()
+//         }
+//         else if(lower.includes(i)){
+//             return k.toLowerCase()()
+//         }
+//         else{
+//             return k
+//         }
+//     }
+// }
+
+// console.log(changeRegister(str))
+
+//5
+// let arr = ["php", "php", "css", "css","script", "script", "html", "html", "java"];
+
+// let removeDuplicates = (arr) =>{
+//     const tempArr = [];
+//     arr.forEach((item) => {
+//         if(!tempArr.includes(item)){
+//             tempArr.push(item);
+//         }
+//     });
+//     return tempArr;
+// };
+
+// console.log(removeDuplicates(arr))
