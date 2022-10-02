d1dc7427f04a3513556c024bc8e2f8fad138a800
Anna Barsukova <Anna Barsukova>
2022-10-02 20:03:52 +0300

    add a

--- src/index.js
+++ src/index.js
@@ -81,7 +81,6 @@
 // console.log(sum(1,2,3))
 
 
-<<<<<<< HEAD
 // const min = (a, b) => {
 //     return (a<b)?a:b
 
@@ -199,71 +198,3 @@ console.log(sumTree(arr));
 // };
 
 // console.log(removeDuplicates(arr))
-=======
-// const min = (a, b) => (a<b)?a:b
-// console.log(min(2,5))
-
-//рекурсия 
-// const sumTo = (n) => {
-//     if (n === 1) return n;
-//     return n + sumTo(n-1) 
-// }
-// console.log(sumTo(4))
-
-
-//замыкание
-// const sum = (a) =>{
-//     return function (b){
-//         return a +b
-//     }
-// }
-// console.log(sum(10)(20));
-
-// const list = {
-//     a: 'a',
-//     b: 'b',
-//     next:{
-//         a: 'a-1',
-//         b: 'b-1',
-//         next:{
-//             a: 'a-2',
-//             b: 'b-2',
-//             next: null
-//         }  
-//     }
-// }
-// const fun = (list) =>{
-//     console.log(list.a)
-//     if (list.next)return fun(list.next)
-//     return; 
-// };
-// fun(list)
-
-// const str = 'aaa bbb ccc'
-
-
-// // console.log(str.substring(4,7))
-// // console.log(str.slice(4,7))
-
-// const search = str.indexOf('bbb');
-// console.log(str.slice(search, search+3))
-
-
-// let array = [2,3,4, undefined, null, '', false, 5,6,0];
-// let arr = array.filter(Boolean)
-// console.log(arr)
-
-
-
-let sumInput = [1,2,3,4,5]
-let sum = sumInput.reduce((a,b) => {
-    return a+b
-},0)
-console.log (sum)
-
-
-
-
-
-
->>>>>>> abaf8ea639078eb26429ee8f84443a4bd47f6ae8
